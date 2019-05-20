7afef99e6d596323280f676310c93bdb1e6ab6ea
kreynertson <kreynertson@psclistens.com>
2019-05-20 09:28:59 -0500

    Add files via upload

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+// app/assets/javascripts/application.js
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
+
+$('.dropdown-toggle').dropdown();
